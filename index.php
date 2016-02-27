46977f3a6cc608870d715dcd7a4bfa114521eccf
Gaurav Sharma <gaurav2728@gmail.com>
2016-02-27 16:49:11 +0530

    setting home page

--- index.php
+++ index.php
@@ -1,4 +1,110 @@
-<h1>This is POS billing program-1 home page</h1>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta name="format-detection" content="telephone=no">
+  <title>Quick Shopping</title>
+  <style type="text/css">
+    a:link {color: inherit; text-decoration: none;}
+  </style>
+</head>
+<body style="margin-top:0; margin-bottom:0; margin-left:0; margin-right:0; padding-top:10px; padding-bottom:30px; padding-left:0; padding-right:0; width:100%; background-color:#f5f5f5;">
+  <table cellspacing="0" cellpadding="0" width="80%" align="center" style="background-color:#FFF; color:#666666; font-family: avenir, Arial, 'Times New Roman', Times, serif;">
+    <tbody>
+      <tr>
+        <td colspan="3" style="padding-top:10px; padding-bottom:10px; background-color:#f5f5f5;" align="left">
+          <img src="imgs/assets/quick_shop_header.png" height="80px;" width="180px;" />
+          <h5>Walk Through in SuperMarket</h5>
+        </td>
+      </tr>
+      <tr style="background-color:#402161; height:3px; font-size:0px;">
+        <td colspan="3">&nbsp;</td>
+      </tr>
+      <tr>
+        <td colspan="3" style="background-color:#e9eaed; color:#000000; padding-top:10px; padding-bottom:10px; padding-left:5%;" align="left">
+        <font size="4">Welcome in Next Generation Shopping</font>
+        <font size="4" style="float: right;">
+          <a href="About" style="margin: 0px 20px 0px 10px;">About</a>
+          <a href="#contact" style="margin: 0px 20px 0px 10px;">Contact</a>
+        </font>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3" style="padding-top:15px; padding-bottom:15px; padding-left:5%; padding-right:5%; line-height:22px;">
+            <font size="3">
+                <p style="float: left;">
+                  <img src="imgs/assets/HomePlus4.jpg" width="500px" height="300px"/>
+                  <br/><font size="5">Products barcode scanning by Customer</font>
+                </p>
+                <p style="float: right;">
+                  <img src="imgs/assets/app.jpeg" width="500px" height="300px"/>
+                  <br/><font size="5">Mobile app showing users selected product list</font>
+                </p>
+            </font>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3" style="padding-top:15px; padding-bottom:15px; padding-left:5%; padding-right:5%; line-height:22px;">
+            <font size="3">
+                <p style="float: left;">
+                  <img src="imgs/assets/HomePlus4.jpg" width="500px" height="300px"/>
+                  <br/><font size="5">Products barcode scanning by Customer</font>
+                </p>
+                <p style="float: right;">
+                  <img src="imgs/assets/app.jpeg" width="500px" height="300px"/>
+                  <br/><font size="5">Mobile app showing users selected product list</font>
+                </p>
+            </font>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3" style="padding-top:15px; padding-bottom:15px; padding-left:5%; padding-right:5%; line-height:22px;">
+            <font size="3">
+                <p style="float: left;">
+                  <img src="imgs/assets/HomePlus4.jpg" width="500px" height="300px"/>
+                  <br/><font size="5">Products barcode scanning by Customer</font>
+                </p>
+                <p style="float: right;">
+                  <img src="imgs/assets/app.jpeg" width="500px" height="300px"/>
+                  <br/><font size="5">Mobile app showing users selected product list</font>
+                </p>
+            </font>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3" style="padding-top:15px; padding-bottom:15px; padding-left:5%; padding-right:5%; line-height:22px;">
+          <a name="contact"></a>
+            <font size="3">
+                <h2 style="margin: 0 20% 0 30%">
+                  <u style="margin: 0 30% 0 30%">Contact</u><br/><br/><br/>
+                  Arpit Dungarwal - 9461025376<br/><br/>
+                  Gaurav D. Sharma - 77377715175<br/><br/>
+                  Gaurav Sharma - 7877056060<br/><br/>
+                  Manyank Pamecha - 8503022199<br/><br/>
+                  Narendra Verma - 9929714264<br/><br/>
+                  Promod Sharma - 9509650603<br/><br/>
+                  Sandeep Kumar Sharma - 8955767558<br/><br/>
+                  Vikram Biwal - 9509299167<br/><br/>
+                </h2>
+            </font>
+        </td>
+      </tr>
+      <tr>
+        <td style="background-color:#FFFFFF; height:1px; font-size:0px; width:5%;">&nbsp;</td>
+        <td style="background-color:#e1e2e3; height:1px; font-size:0px; width:90%;">&nbsp;</td>
+        <td style="background-color:#FFFFFF; height:1px; font-size:0px; width:5%;">&nbsp;</td>
+      </tr>
+      <tr>
+        <td style="background-color:#FFFFFF; height:1px; font-size:0px; width:5%;">&nbsp;</td>
+        <td style="padding-top:5px; color:#CCCCCC; width:90%;" align="center"><font size="1">&copy; Qucik Shop Team | All rights reserved</font></td>
+        <td style="background-color:#FFFFFF; height:1px; font-size:0px; width:5%;">&nbsp;</td>
+      </tr>
+      <tr>
+        <td colspan="3" align="center">&nbsp;</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
+
 <?php
-phpinfo();
+// phpinfo();
 ?>
